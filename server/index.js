cfb9123586066340171f67e980f5f4b074e3563d
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-04-13 17:03:38 +0200

    Add WebSocket server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require(`express`);
 const path = require(`path`);
+const webSocket = require(`ws`);
+const http = require(`http`);
 
 const news = require(`./data/news.json`);
+const news3add = require(`./data/news-3-add.json`);
+const news1Update = require(`./data/news-1-update`);
 
 const PORT = 8200;
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `*`);
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
+  next();
+});
 
 app.use(express.static(path.resolve(__dirname, `components`), {
   maxAge: `365d`,
@@ -15,6 +25,27 @@ app.get(`/news`, (req, res) => {
   res.send(news);
 });
 
-app.listen(PORT);
+const server = http.createServer(app);
+
+const wss = new webSocket.Server({
+  path: `/ws`,
+  server,
+});
+
+const ADD = JSON.stringify({
+  type: `ADD`,
+  entity: news3add,
+});
+const UPDATE = JSON.stringify({
+  type: `UPDATE`,
+  entity: news1Update,
+});
+
+wss.on(`connection`, (ws) => {
+  setInterval(() => ws.send(ADD), 5000);
+  setTimeout(() => setInterval(() => ws.send(UPDATE), 5000), 2500);
+});
+
+server.listen(PORT);
 // eslint-disable-next-line no-console
 console.log(`Listening on: http://localhost:${PORT}`);
