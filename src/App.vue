a739c9a1be7aae9cd6f8d5a8566d9b5a015f7c47
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-04-14 09:40:45 +0200

    Minor styling enhancements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="o-container o-container--s o-vertical-spacing o-vertical-spacing--xl"
+    class="o-container o-container--m o-vertical-spacing o-vertical-spacing--xxl"
     :class="$style.main"
   >
     <h1>
@@ -25,18 +25,19 @@ export default {
 @import './assets/scss/generic/**/*';
 @import '{
   .o-container,
-  .o-container--s,
+  .o-container--m,
 } from ~@avalanche/object-container';
 @import '{
   .o-vertical-spacing,
   .o-vertical-spacing--xl,
+  .o-vertical-spacing--xxl,
 } from ~@avalanche/object-vertical-spacing';
 </style>
 
 <style lang="scss" module>
 @import './assets/scss/settings/**/*';
 
-.App {
+.main {
   padding-top: setting-spacing(xl);
   padding-bottom: setting-spacing(xl);
 
